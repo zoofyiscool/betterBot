3c42e83ae5e0fba9b40be8448b5a4c56d71f0191
zoofyiscool <75759609+zoofyiscool@users.noreply.github.com>
2020-12-13 01:42:34 +0000

    version 0.1

---
+++ betterBot/betterBot/Modules/Commands.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+// using Discord;
+// using Discord.WebSocket;
+
+namespace betterBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        // help command
+
+        [Command("help")]
+        public async Task Help()
+        {
+            await ReplyAsync("``` Commands: \n 1. bb!ping \n 2. bb!supercoolfact ```");
+        }
+
+        // simple ping command
+
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync(Context.Message.Author.Mention + " Pong!");
+           // await message.Author.SendMessageAsync("Poggers");
+        }
+
+        // super cool fact
+
+        [Command("supercoolfact")]
+        public async Task SuperCoolFact()
+        {
+            await ReplyAsync("your not cool");
+        }
+    }
+}
